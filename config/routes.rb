61a340f3e3afe880ea9c295a2b2d3fde8bb8d220
EC2 Default User <ec2-user@ip-172-31-1-16.ap-northeast-1.compute.internal>
2024-12-31 01:34:13 +0000

    [Start]ルートの初期設定

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  root 'homes#top'
+  #root_pathと記述することでviewで使用できる
+  resources :books
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
